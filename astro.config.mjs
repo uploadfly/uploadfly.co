ce07251193d9f6a09f811c3d451668a498cabf87
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-27 14:23:47 +0100

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 1900,
-    host: true,
+    host: true
   },
+  integrations: [tailwind()]
 });
