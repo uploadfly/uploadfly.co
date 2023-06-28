ecf780b67157ee9ac7700c690266eaed710a45b6
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-28 14:02:55 +0100

    setup tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        ["uf-dark"]: "#0b0b0b",
+        ["uf-light"]: "#eeecee",
+        ["uf-accent"]: "#0B6CEC",
+      },
+    },
+  },
+  plugins: [],
+};
