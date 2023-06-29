4e0bcc13e62b72cf06764568e69faae1cd09b4ba
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-29 12:55:55 +0100

    added Features and UpTime components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import UpTime from "@/components/UpTime";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Features />
+      <UpTime />
     </>
   );
 }
