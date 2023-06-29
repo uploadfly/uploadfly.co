366556cdab90a1d442ef1b1af11e4160af6faf0e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-29 12:57:05 +0100

    initial commit

---
+++ components/UpTime.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UpTime = () => {
+  return (
+    <div className="my-20">
+      <h1 className="shiny-text text-center text-4xl">
+        99.99999999999% up time
+      </h1>
+      <p className="text-slate-200 text-center text-xl w-[90%] mx-auto mt-3">
+        Delivery through 450+ global CDN locations ensuring consistent and rapid
+        content delivery for your users, anytime, anywhere.
+      </p>
+    </div>
+  );
+};
+
+export default UpTime;
