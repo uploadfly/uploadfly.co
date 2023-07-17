d447a3bf509735b6b245ca4ff067a4d8a789258f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-17 22:52:49 +0100

    setup redirections

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/chat" || "/community" || "/discord",
+        destination: "https://discord.gg/5crMTTt6",
+        permanent: true,
+      },
+      {
+        source: "/github" || "/opensource",
+        destination: "https://github.com/uploadfly",
+        permanent: true,
+      },
+      {
+        source: "/docs",
+        destination: "https://docs.uploadfly.cloud",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
