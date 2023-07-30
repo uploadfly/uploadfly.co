cee7a5b6157154991942db86aa96a87370bc9fbc
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-30 18:13:53 +0100

    added plausible analytics

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import TechStack from "@/components/TechStack";
 import UpTime from "@/components/UpTime";
+import PlausibleProvider from "next-plausible";
 
 export default function Home() {
   return (
-    <>
+    <PlausibleProvider domain="uploadfly.cloud">
       <Navbar />
       <Hero />
       <Features />
       <TechStack />
       <UpTime />
-    </>
+    </PlausibleProvider>
   );
 }
