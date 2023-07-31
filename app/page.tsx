ec592d90dabbb337d5b0fe843cef8a59196ff270
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-31 17:15:31 +0100

    Commented out TechStack component

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
-      <TechStack />
+      {/* <TechStack /> */}
       <UpTime />
     </PlausibleProvider>
   );
