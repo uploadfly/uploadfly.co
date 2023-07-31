a5a4b5b7bf5cb01ebc6cd99418a232a606b7d254
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-31 18:34:21 +0100

    moved page tracking to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import PlausibleProvider from "next-plausible";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <PlausibleProvider domain="uploadfly.cloud" trackOutboundLinks>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </PlausibleProvider>
   );
 }
