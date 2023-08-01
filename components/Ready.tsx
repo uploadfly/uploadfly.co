7d500530be7f2564b388d88adb725c4c51a92645
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-01 20:42:14 +0100

    fixed dark mode

--- components/Ready.tsx
+++ components/Ready.tsx
@@ -1,23 +1,29 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Ready = () => {
   return (
-    <div className="flex items-center flex-col gap-5 py-36"
-    style={{
+    <div
+      className="flex items-center flex-col gap-5 py-36"
+      style={{
         backgroundImage:
           "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(/grid-bg.png)",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-        <h1 className="text-6xl lg:w-[50%] mb-5 text-center font-semibold">Start uploading with Uploadfly today.</h1>
+      <h1 className="text-6xl lg:w-[50%] mb-5 text-center font-semibold">
+        Start uploading with Uploadfly today.
+      </h1>
 
-<Link href={"https://beta.uploadfly.cloud/signup"} target="_blank" className="bg-accent px-10 py-3 font-semibold text-lg rounded-full">
-    Get started
-</Link>
-
+      <Link
+        href={"https://beta.uploadfly.cloud/signup"}
+        target="_blank"
+        className="bg-accent px-10 py-3 font-semibold text-lg rounded-full"
+      >
+        Get started
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Ready
+export default Ready;
