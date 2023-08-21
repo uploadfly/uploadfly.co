5564a1e3572a2ca6a7843e1ff9285618fdfc4771
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 13:01:34 +0100

    initial commit

---
+++ contentlayer.config.ts
@@ -0,0 +1,40 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const ChangelogPost = defineDocumentType(() => ({
+  name: "ChangelogPost",
+  filePathPattern: `**/changelog/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "string",
+      required: true,
+    },
+    tag: {
+      type: "string",
+    },
+    summary: {
+      type: "string",
+      required: true,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (changelogPost) =>
+        `/changelog/${changelogPost._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "content",
+  documentTypes: [ChangelogPost],
+});
