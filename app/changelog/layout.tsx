16f181968b4e29a605b961dc7af179652658cdd8
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 03:51:02 +0100

    created changelog layout

---
+++ app/changelog/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Changelog  • Uploadfly",
+};
+
+const ChangelogLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default ChangelogLayout;
