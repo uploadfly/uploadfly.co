e82cd08d9639eee4fab56675ba5530abef47a9c3
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 16:42:37 +0100

    added footer

--- app/changelog/layout.tsx
+++ app/changelog/layout.tsx
@@ -1,9 +1,16 @@
+import Footer from "@/components/Footer";
+
 export const metadata = {
   title: "Changelog  • Uploadfly",
 };
 
 const ChangelogLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default ChangelogLayout;
