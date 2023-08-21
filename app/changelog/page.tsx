2ed68e73567862a56831160956a75e1ef4b73efb
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 03:50:29 +0100

    created page.tsx for changelog

---
+++ app/changelog/page.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+
+const Changelog = () => {
+  return (
+    <div>
+      <div className="border-b border-white/20 w-full">
+        <Navbar />
+      </div>
+      <div
+        className="w-full flex items-center justify-center flex-col py-32 gap-3"
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(/grid-bg.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <h1 className="text-white text-6xl font-bold">Changelog</h1>
+        <p>All the new stuff with Uploadfly</p>
+      </div>
+    </div>
+  );
+};
+
+export default Changelog;
