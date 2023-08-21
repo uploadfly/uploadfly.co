a24855b40288e50c71ba1b9dafeaf0e431184088
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 16:09:17 +0100

    initial commit

--- app/changelog/[slug]/page.tsx
+++ app/changelog/[slug]/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Navbar from "@/components/Navbar";
+import { allChangelogPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
-const Slug = () => {
-  return <div>Slug</div>;
+export async function generateStaticParams() {
+  return allChangelogPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata | undefined> {
+  const post = allChangelogPosts.find((post) => post.slug === params.slug);
+  if (!post) {
+    return;
+  }
+
+  const { title, summary: description } = post;
+}
+
+const ChangelogPost = ({ params }: { params: { slug: string } }) => {
+  const post = allChangelogPosts.find(
+    (post) => post.slug.replace("\r", "") === params.slug
+  );
+
+  if (!post) {
+    notFound();
+  }
+  return (
+    <div>
+      <div className="border-b border-white/20 w-full">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
-export default Slug;
+export default ChangelogPost;
