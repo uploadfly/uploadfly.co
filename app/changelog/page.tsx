9b5bb9d831bc332dd715cc56eca0b22a15a16b6b
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 13:02:03 +0100

    mapped posts

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
-import { allPosts } from "";
+import { allChangelogPosts, ChangelogPost } from "contentlayer/generated";
 import { useLiveReload } from "next-contentlayer/hooks";
 
 const Changelog = () => {
-  useLiveReload();
+  // useLiveReload();
   return (
     <div>
       <div className="border-b border-white/20 w-full">
@@ -20,6 +22,23 @@ const Changelog = () => {
       >
         <h1 className="text-white text-6xl font-bold">Changelog</h1>
         <p>All the new stuff with Uploadfly...</p>
+      </div>
+      <div className="flex items-center flex-col w-[60%] mx-auto my-10">
+        {allChangelogPosts.map((post: ChangelogPost) => (
+          <div className="border-b border-accent/50 py-3">
+            <div className="">
+              <p className="mb-5 font-semibold text-accent/80">{post.tag}</p>
+              <p className="text-sm font-semibold">
+                {new Date(post.publishedAt).toDateString()}
+              </p>
+              <h1 className="text-3xl font-bold">{post.title}</h1>
+              <div
+                className="text-lg mt-3"
+                dangerouslySetInnerHTML={{ __html: post.body.raw }}
+              ></div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
