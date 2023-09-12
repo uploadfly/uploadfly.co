6952d5a3feab4cc0f20a750bd500b17813f9ae2f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-12 11:07:02 +0100

    [Updated Footer, Uptime component] - Adjusted footer to get currentYear - Updated footer backdrop image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,11 @@ import Link from "next/link"
 import { SiGithub, SiTwitter } from "react-icons/si"
 
 const Footer = () => {
+    const date = new Date();
+    const currentYear = date.getFullYear();
   return (
     <div className="flex items-center justify-between px-5 py-5">
-    <small>Uploadfly • 2023</small>
+    <p className="capitalize">Uploadfly • {currentYear}</p>
         <div className="flex items-center gap-4 text-xl">
             <Link href={"https://x.com/uploadflycloud"} target="_blank">
                 <SiTwitter/>
--- components/Ready.tsx
+++ components/Ready.tsx
@@ -6,19 +6,20 @@ const Ready = () => {
       className="flex items-center flex-col gap-5 py-36"
       style={{
         backgroundImage:
-          "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(/grid-bg.png)",
+          "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(/footer-backdrop.svg)",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <h1 className="text-6xl lg:w-[50%] mb-5 text-center font-semibold">
-        Start uploading with Uploadfly today.
+      <h1 className="text-3xl p-1 md:text-5xl lg:w-[50%] mb-5 text-center font-semibold my-5">
+        Start uploading with Uploadfly 
+        today.
       </h1>
 
       <Link
         href={"https://beta.uploadfly.cloud/signup"}
         target="_blank"
-        className="bg-accent px-10 py-3 font-semibold text-lg rounded-full"
+        className="bg-accent px-10 py-3 font-semibold text-lg rounded-full my-5"
       >
         Get started
       </Link>
