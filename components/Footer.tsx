3aabc6926b563d42120ea59be993977b6a93d59e
Akinkunmi <akinkunmioye42@gmail.com>
2024-01-04 15:44:06 +0100

    fix: get current year with JavaScript.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { SiGithub, SiTwitter } from "react-icons/si";
 const Footer = () => {
   return (
     <div className="flex items-center justify-between px-5 py-5">
-      <small>UploadFly • 2023</small>
+      <small>UploadFly • {new Date().getFullYear()}</small>
       <div className="flex items-center gap-4 text-xl">
         <Link href={"https://x.com/uploadflycloud"} target="_blank">
           <SiTwitter />
